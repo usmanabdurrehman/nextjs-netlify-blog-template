2945fc4a248d4082df56a4b8adefef6139076fb0
Usman Abdur Rehman <saidskit@outlook.com>
2021-07-28 21:12:37 +0500

    Patch Version 3

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -25,7 +25,7 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="root" className={classes.root}>
+    <div className={classes.root}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
